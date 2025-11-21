cb030db90d42b0c3010c7bea5768edc16245ac20
John Lyold Lozada <lozadajohnlyoldcastro12705@gmail.com>
2025-11-22 01:23:18 +0800

    Initial commit

---
+++ android/build.gradle.kts
@@ -0,0 +1,40 @@
+
+plugins {
+    // Root plugins (none needed specifically for google services when using buildscript classpath)
+}
+
+// Legacy buildscript block to expose Google Services plugin for `apply(plugin = "com.google.gms.google-services")`
+buildscript {
+        repositories {
+                google()
+                mavenCentral()
+        }
+        dependencies {
+                classpath("com.google.gms:google-services:4.4.0")
+        }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:attendanceapp/homescreen.dart';
+import 'package:attendanceapp/loginscreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: KeyboardVisibilityProvider(child: AuthCheck()),
+    );
+  }
+}
+
+class AuthCheck extends StatefulWidget {
+  const AuthCheck({Key? key}) : super(key: key);
+
+  @override
+  _AuthCheckState createState() => _AuthCheckState();
+}
+
+class _AuthCheckState extends State<AuthCheck> {
+  bool userAvailable = false;
+  late SharedPreferences sharedPreferences;
+  @override
+  void initState() {
+    super.initState();
+
+    _getCurrentUser();
+  }
+
+  void _getCurrentUser() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+
+    try {
+      final storedId = sharedPreferences.getString('studentId');
+      setState(() {
+        userAvailable = storedId != null && storedId.isNotEmpty;
+      });
+    } catch (e) {
+      setState(() {
+        userAvailable = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return userAvailable ? const Homescreen() : const LoginScreen();
+  }
+}
